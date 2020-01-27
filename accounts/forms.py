ff9b661504174d1b8022deb84c448ba82f5dbf1a
Joanna Broks <asia.ostr@gmail.com>
2020-01-27 17:15:20 +0000

    First and lastname added to the registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,25 +14,64 @@ class UserLoginForm(forms.Form):
 class UserRegistrationForm(UserCreationForm):
     """Form used to register a new user"""
 
+        
+    first_name = forms.CharField(
+        label="First name",
+        min_length=2,
+        max_length=50,
+        widget=forms.TextInput,
+        required=True)
+    
+    last_name = forms.CharField(
+        label="Last name",
+        min_length=2,
+        max_length=50,
+        widget=forms.TextInput,
+        required=True) 
+    
+    email = forms.CharField(
+        label="Email",
+        min_length=2,
+        max_length=50,
+        widget=forms.EmailInput,
+        required=True) 
+
+    username = forms.CharField(
+        label="Username",
+        min_length=5,
+        max_length=25,
+        widget=forms.TextInput,
+        required=True) 
+
     password1 = forms.CharField(
         label="Password",
+        min_length=5,
+        max_length=50,
         widget=forms.PasswordInput)
+        
     password2 = forms.CharField(
         label="Password Confirmation",
+        min_length=5,
+        max_length=50,
         widget=forms.PasswordInput)
     
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
         
     def clean_email(self):
+        """ Check if email is already registered """
+        
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
+        
         if User.objects.filter(email=email).exclude(username=username):
             raise forms.ValidationError(u'Email address must be unique')
         return email
 
     def clean_password2(self):
+        """ Check that the two password entries match """
+        
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
 
