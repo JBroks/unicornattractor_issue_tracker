74c8c82b565095516308870e8d2de13eb241b050
Joanna Broks <asia.ostr@gmail.com>
2020-02-17 11:36:45 +0000

    Index url added

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from accounts.views import index
 from accounts import urls as urls_accounts
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name="index"),
     url(r'^accounts/', include(urls_accounts)),
+    
 ]
