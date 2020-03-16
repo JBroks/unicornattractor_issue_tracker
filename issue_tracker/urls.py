57693630a2f989dd43fffa22d85fb9b0ed9872b9
Joanna Broks <asia.ostr@gmail.com>
2020-03-16 14:54:06 +0000

    Tickets app added to installed apps. Urls added

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import index
 from accounts import urls as urls_accounts
+from tickets import urls as urls_tickets
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -24,5 +25,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(urls_accounts)),
+    url(r'^tickets/', include(urls_tickets)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
