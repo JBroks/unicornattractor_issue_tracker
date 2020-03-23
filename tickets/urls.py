e7fd62c9af213dc8177d518d09408efa6d111ba3
Joanna Broks <asia.ostr@gmail.com>
2020-03-23 15:47:39 +0000

    Urls, views file template, and html templates set up for the tickets app

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url, include
+from tickets.views import add_ticket, all_tickets, edit_ticket, view_ticket
+#delete_ticket
 
 urlpatterns = [
-    
+    url(r'^tickets-all/', all_tickets, name="all_tickets"),
+    url(r'^ticket-add/', add_ticket, name="add_ticket"),
+    url(r'^ticket-edit/', edit_ticket, name="edit_ticket"),
+    url(r'^ticket-view/', view_ticket, name="view_ticket"),
+    #url(r'^ticket-delete/', delete_ticket, name="delete_ticket"),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def add_ticket(request):
+    return render(request, 'add_ticket.html')
+    
+def all_tickets(request):
+    return render(request, 'all_tickets.html')
+    
+def edit_ticket(request):
+    return render(request, 'edit_ticket.html')
+
+def view_ticket(request):
+    return render(request, 'view_ticket.html')
+'''
+def delete_ticket(request):
+'''
