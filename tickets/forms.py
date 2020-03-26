669098087004ac59d2d35aae1412a80f2375dfe2
Joanna Broks <asia.ostr@gmail.com>
2020-03-26 14:43:39 +0000

    Add ticket form created

---
+++ tickets/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Ticket
+from django.core.exceptions import ValidationError
+
+class AddTicket(forms.Form):
+    '''
+    
+    '''
+    TYPE_CHOICES = [(i,i) for i in ["Feature", "Bug"] ]
+    
+    ticket_type = forms.ChoiceField(
+        label='Ticket type',
+        choices=TYPE_CHOICES, 
+        required=False)
+        
+    subject = forms.CharField(
+        label="Subject",
+        min_length=5,
+        max_length=100,
+        widget=forms.TextInput(),
+        required=True)
+        
+    description = forms.CharField(
+        label="Description",
+        min_length=20,
+        max_length=3000,
+        widget=forms.Textarea(),
+        required=True)
+    
+    class Meta:
+        model = Ticket
+        fields = ["ticket_type", "subject", "description"]
