26848538d8c384670950a7b271f92119f4223daa
Joanna Broks <asia.ostr@gmail.com>
2020-04-05 13:03:37 +0000

    Add a new ticket form created

--- tickets/forms.py
+++ tickets/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Ticket
 from django.core.exceptions import ValidationError
 
-class AddTicket(forms.Form):
+class AddTicketForm(forms.ModelForm):
     '''
     
     '''
