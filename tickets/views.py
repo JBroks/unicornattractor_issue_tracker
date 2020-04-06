19cbdb4e3c53437d85824bdff05d4cf8e6d25cf9
Joanna Broks <asia.ostr@gmail.com>
2020-04-06 14:56:26 +0000

    All tickets page and ticket pagination

--- tickets/views.py
+++ tickets/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from tickets.models import Ticket
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from tickets.forms import AddTicketForm
+
 
 # Create your views here.
 
@@ -27,7 +30,24 @@ def add_ticket(request):
     return render(request, 'add_ticket.html', {'add_ticket_form': add_ticket_form} )
    
 def all_tickets(request):
-    return render(request, 'all_tickets.html')
+    '''
+    View all tickets in a form of paginated table.
+    Enable user to filter tickets by type, status and / or user who added it
+    '''
+    
+    tickets_list = Ticket.objects.all()
+    page = request.GET.get('page', 1)
+    
+    # Paginate tickets
+    paginator = Paginator(tickets_list, 1)
+    try:
+        tickets = paginator.page(page)
+    except PageNotAnInteger:
+        tickets = paginator.page(1)
+    except EmptyPage:
+        tickets = paginator.page(paginator.num_pages)
+        
+    return render(request, 'all_tickets.html', {'tickets': tickets} )
     
 def edit_ticket(request):
     return render(request, 'edit_ticket.html')
