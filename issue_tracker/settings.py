82d0c4ec6571bdd748aeb526c6896449ff4144a5
Joanna Broks <asia.ostr@gmail.com>
2020-04-07 15:46:32 +0000

    Spelling error amended in the settings file

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'django_filter',
+    'django_filters',
     'accounts',
     'home',
     'storages',
