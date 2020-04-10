9dda152e8ad6a2d1f857d2e72345566b541329b2
Joanna Broks <asia.ostr@gmail.com>
2020-04-10 14:34:27 +0000

    Reset filters button added

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
+// TOOLTIPS
+/**
+ * Function to initialize all tooltips on a page
+ **/
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+
+// RESET TICKETS FILTERS
+/** 
+ * Onclick functions for the ticket type and status filters.
+ * Function that clears the filters after reset button is clicked by a user
+ **/
+$('#reset').click(function(){
+    $("#ticket_type").val($("#ticket_type option:first").val());
+    $("#ticket_status").val($("#ticket_status option:first").val());
+});
