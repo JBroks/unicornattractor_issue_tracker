52c75a2aa2239871ec97433dc8351c464882a1ce
Joanna Broks <asia.ostr@gmail.com>
2020-04-21 16:17:13 +0000

    Delete ticket url added

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url, include
-from tickets.views import add_or_edit_ticket, all_tickets, view_ticket
-#delete_ticket
+from tickets.views import add_or_edit_ticket, all_tickets, view_ticket, delete_ticket
 
 urlpatterns = [
     url(r'^tickets-all/', all_tickets, name="all_tickets"),
     url(r'^new/$', add_or_edit_ticket, name="add_ticket"),
     url(r'^(?P<pk>\d+)/edit/$', add_or_edit_ticket, name="edit_ticket"),
     url(r'^(?P<pk>\d+)/view/$', view_ticket, name="view_ticket"),
-    #url(r'^ticket-delete/', delete_ticket, name="delete_ticket"),
+    url(r'^(?P<pk>\d+)/ticket-delete/$', delete_ticket, name="delete_ticket"),
 ]
