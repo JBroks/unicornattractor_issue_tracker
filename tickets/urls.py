d858b1ce1f76ee0852c1a110fc73a1e465178f64
Joanna Broks <asia.ostr@gmail.com>
2020-04-21 13:23:07 +0000

    View single ticket view created and url added

--- tickets/urls.py
+++ tickets/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^tickets-all/', all_tickets, name="all_tickets"),
     url(r'^new/$', add_or_edit_ticket, name="add_ticket"),
     url(r'^(?P<pk>\d+)/edit/$', add_or_edit_ticket, name="edit_ticket"),
-    url(r'^ticket-view/', view_ticket, name="view_ticket"),
+    url(r'^(?P<pk>\d+)/view/$', view_ticket, name="view_ticket"),
     #url(r'^ticket-delete/', delete_ticket, name="delete_ticket"),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -11,7 +11,15 @@ from django.db.models import Count
 
 @login_required
 def add_or_edit_ticket(request, pk=None):
+    '''
+    Allows user to add a new ticket or edit an existing one
+    If user wants to add a new ticket he or she is redirected to a blank
+    add_ticket_form where all details can be supplied and submitted
+    If user wants to edit the ticket they are redirected to the ticket form
+    that is pre-filled with ticket details
+    '''
     
+    # Retrive the ticket if exists
     ticket = get_object_or_404(Ticket, pk=pk) if pk else None
     
     if request.method == "POST":
@@ -36,6 +44,7 @@ def all_tickets(request):
     '''
     View all tickets in a form of paginated table.
     Enable user to filter tickets by type and / or status
+    Table is paginated to show only 10 records per page
     '''
     
     # Create type and status list for select option menu
@@ -77,12 +86,20 @@ def all_tickets(request):
             'status_list': status_list }
         
     return render(request, 'all_tickets.html', args )
-       
-def edit_ticket(request):
-    return render(request, 'edit_ticket.html')
 
-def view_ticket(request):
-    return render(request, 'view_ticket.html')
+def view_ticket(request, pk):
+    '''
+    Enables user to view page containing all details regarding a selected ticket
+    Function retrieves a single ticket based on its ID (pk) and renders it to 
+    the view_ticket.html template
+    If object is not found 404 error is being returned
+    '''
+    
+    # Retrive the ticket
+    ticket = get_object_or_404(Ticket, pk=pk)
+    
+    return render(request, 'view_ticket.html', {'ticket': ticket})
+    
 '''
 def delete_ticket(request):
 '''
