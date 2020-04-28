4037fccc6ea61d9f0690a904bcdc949a86b5ddf0
Joanna Broks <asia.ostr@gmail.com>
2020-04-28 11:07:41 +0000

    Donation and Upvote model registered

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from tickets.models import Ticket
+from tickets.models import Ticket, Upvote, Donation
 
 # Register your models here.
 
 admin.site.register(Ticket)
+admin.site.register(Upvote)
+admin.site.register(Donation)
