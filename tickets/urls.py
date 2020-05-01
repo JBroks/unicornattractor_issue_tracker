9a7a3236233b5576d92617743d2c4f1b65c6100e
Joanna Broks <asia.ostr@gmail.com>
2020-05-01 09:55:49 +0000

    Url added for upvote view

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from tickets.views import add_or_edit_ticket, all_tickets, view_ticket, delete_ticket
+from tickets.views import add_or_edit_ticket, all_tickets, view_ticket, delete_ticket, upvote
 
 urlpatterns = [
     url(r'^tickets-all/', all_tickets, name="all_tickets"),
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/edit/$', add_or_edit_ticket, name="edit_ticket"),
     url(r'^(?P<pk>\d+)/view/$', view_ticket, name="view_ticket"),
     url(r'^(?P<pk>\d+)/ticket-delete/$', delete_ticket, name="delete_ticket"),
+    url(r'^(?P<pk>\d+)/upvote/$', upvote, name="upvote"),
 ]
