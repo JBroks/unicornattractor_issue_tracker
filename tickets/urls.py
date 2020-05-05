3ee6d0703a05e72fdef264a081319b44ce3bc4a0
Joanna Broks <asia.ostr@gmail.com>
2020-05-05 17:47:06 +0000

    urls updated

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     url(r'^tickets-all/', all_tickets, name="all_tickets"),
     url(r'^new/$', add_or_edit_ticket, name="add_ticket"),
     url(r'^(?P<pk>\d+)/edit/$', add_or_edit_ticket, name="edit_ticket"),
-    url(r'^(?P<pk>\d+)/view/$', view_ticket, name="view_ticket"),
-    url(r'^(?P<pk>\d+)/ticket-delete/$', delete_ticket, name="delete_ticket"),
-    url(r'^(?P<pk>\d+)/upvote/$', upvote, name="upvote"),
-    url(r'^(?P<pk>\d+)/downvote/$', downvote, name="downvote"),
-    url(r'^(?P<pk>\d+)/edit_comment/$', add_or_edit_comment, name="edit_comment"),
-    url(r'^(?P<pk>\d+)/add_comment/$', add_or_edit_comment, name="add_comment")
+    url(r'^view_ticket/(?P<pk>\d+)/$', view_ticket, name="view_ticket"),
+    url(r'^ticket-delete/(?P<pk>\d+)/$', delete_ticket, name="delete_ticket"),
+    url(r'^upvote/(?P<pk>\d+)/$', upvote, name="upvote"),
+    url(r'^downvote/(?P<pk>\d+)/$', downvote, name="downvote"),
+    url(r'^edit_comment/(?P<pk>\d+)/$', add_or_edit_comment, name="edit_comment"),
+    url(r'^new_comment/$', add_or_edit_comment, name="add_comment")
 ]
