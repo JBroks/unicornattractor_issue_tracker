470ed042047b0f4ffd6d0aacd9b5f2a77b07bf59
Joanna Broks <asia.ostr@gmail.com>
2020-05-06 10:05:54 +0000

    Ticket urls tidy up and comments - add ad edit urls added

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url, include
 from tickets.views import add_or_edit_ticket, all_tickets, view_ticket, delete_ticket, upvote, downvote, add_or_edit_comment
 
 urlpatterns = [
-    url(r'^tickets-all/', all_tickets, name="all_tickets"),
+    url(r'^$', all_tickets, name="all_tickets"),
     url(r'^new/$', add_or_edit_ticket, name="add_ticket"),
-    url(r'^(?P<pk>\d+)/edit/$', add_or_edit_ticket, name="edit_ticket"),
-    url(r'^view_ticket/(?P<pk>\d+)/$', view_ticket, name="view_ticket"),
-    url(r'^ticket-delete/(?P<pk>\d+)/$', delete_ticket, name="delete_ticket"),
-    url(r'^upvote/(?P<pk>\d+)/$', upvote, name="upvote"),
-    url(r'^downvote/(?P<pk>\d+)/$', downvote, name="downvote"),
-    url(r'^edit_comment/(?P<pk>\d+)/$', add_or_edit_comment, name="edit_comment"),
-    url(r'^new_comment/$', add_or_edit_comment, name="add_comment")
+    url(r'^edit/ticket/(?P<pk>\d+)/$', add_or_edit_ticket, name="edit_ticket"),
+    url(r'^delete/ticket/(?P<pk>\d+)/$', delete_ticket, name="delete_ticket"),
+    url(r'^upvote/ticket/(?P<pk>\d+)/$', upvote, name="upvote"),
+    url(r'^downvote/ticket/(?P<pk>\d+)/$', downvote, name="downvote"),
+    url(r'^view/ticket/(?P<pk>\d+)/$', view_ticket, name="view_ticket"),
+    url(r'^view/ticket/(?P<ticket_pk>\d+)/comments/new/$', add_or_edit_comment, name="add_comment"),
+    url(r'^view/ticket/(?P<ticket_pk>\d+)/comments/edit/(?P<pk>\d+)/$', add_or_edit_comment, name="add_comment")
 ]
