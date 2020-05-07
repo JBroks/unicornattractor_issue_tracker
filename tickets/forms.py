3c96f981d3697acec3bed6fb8647964e4c9713b8
Joanna Broks <asia.ostr@gmail.com>
2020-05-07 14:15:44 +0000

    Comment form rows number increased

--- tickets/forms.py
+++ tickets/forms.py
@@ -95,7 +95,7 @@ class AddCommentForm(forms.ModelForm):
         label="Leave a comment",
         min_length=20,
         max_length=8000,
-        widget=forms.Textarea(attrs={'rows':2, 'cols':10, 'placeholder': 'Add a comment...'}),
+        widget=forms.Textarea(attrs={'rows':3, 'cols':10, 'placeholder': 'Add a comment...'}),
         required=True)
     
     class Meta:
