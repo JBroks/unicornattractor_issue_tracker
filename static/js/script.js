25cbf322a0e7b67968617b1a0d3a6f92d1e57e07
Joanna Broks <asia.ostr@gmail.com>
2020-05-09 14:07:09 +0000

    Read more/less buttons added and js function added

--- static/js/script.js
+++ static/js/script.js
@@ -23,6 +23,8 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
   e.relatedTarget // previous active tab
 });
 
+// DISPLAY EDIT COMMENT FORM
+
 /** 
  * Function that displays only the comment form which user chose to update.
  * Function is initiated on edit button click.
@@ -36,5 +38,54 @@ $(document).ready(function() {
   $('.edit-comment-btn').click(function() {
     $(this).parents('div.card-header').nextAll('.posted-comment').hide();
     $(this).parents('div.card-header').nextAll('.edit-comment').show();
+  });
+});
+
+// CHECK TRUNCATE
+
+/**
+ * Function that checks which comments were truncated in order to display
+ * 'read more' link only for those comments
+ **/
+
+$(function() {
+     $('.comment-text').each(function(index, elem) {
+
+         if(elem.offsetWidth === elem.scrollWidth){
+          	$(this).siblings('.read-more').hide();
+         }
+     });
+ });
+ 
+ // SHOW MORE / LESS COMMENT
+
+/**
+ * Function that toggles materialize class truncate for the comment text
+ * in order to manage very long comments.
+ * When comments are truncated link 'more' can be clicked to see full text.
+ * On other other hand when user wants to hide the comment again he / she can 
+ * click 'show less' which will truncate the comment 
+ **/
+
+// Show more
+$(document).ready(function() {
+  $(function() {
+    $('.read-less').hide();
+    $('.read-more').click(function() {
+      $(this).siblings('.comment-text').toggleClass('text-truncate');
+      $(this).hide();
+      $(this).siblings('.read-less').show();
+    });
+  });
+});
+
+// Show less
+$(document).ready(function() {
+  $(function() {
+    $('.read-less').click(function() {
+      $(this).siblings('.comment-text').toggleClass('text-truncate');
+      $(this).hide();
+      $(this).siblings('.read-more').show();
+    });
   });
 });
