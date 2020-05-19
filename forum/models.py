f6255d1e4bf8bd080a7d2e1ecde8b37720cb160a
Joanna Broks <asia.ostr@gmail.com>
2020-05-19 15:44:19 +0000

    Comment model added and registered

--- forum/models.py
+++ forum/models.py
@@ -39,5 +39,43 @@ class Post(models.Model):
         ordering = ['-id']
         
     def __str__(self):
-        return "Forum post #{0} [{1}] by {2}".format(
-            self.id, self.subject, self.user)
+        return "Forum post #{0} [{1}] added by {2}".format(
+            self.id, self.subject, self.user.username)
+
+class Comment(models.Model):
+    '''
+    Enables users to comment on a given forum post, express their opinions,
+    and be a part of the community
+    '''
+    
+    post = models.ForeignKey(
+        Post,
+        null=True,
+        on_delete=models.CASCADE)
+    
+    user = models.ForeignKey(
+        User, 
+        null=True, 
+        on_delete=models.CASCADE)
+        
+    comment = models.TextField(
+        max_length=8000,
+        blank=False)
+        
+    date_created = models.DateTimeField(
+        blank=False,
+        null=False,
+        auto_now_add=True)
+        
+    date_updated = models.DateTimeField(
+        blank=False,
+        null=False,
+        auto_now=True,
+        )
+        
+    class Meta:
+        ordering = ['-date_created']
+        
+    def __str__(self):
+        return "Comment #{0} added by {1} for post #{2}".format(
+            self.id, self.user.username, self.post.id)
