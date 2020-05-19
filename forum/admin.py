d17a280d76ebef2a613c2a93b3069c1174ec604e
Joanna Broks <asia.ostr@gmail.com>
2020-05-19 15:48:10 +0000

    MOdel name changed

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from forum.models import Post, Comment
+from forum.models import Post, CommentPost
 
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(CommentPost)
