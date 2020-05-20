2b3f5508b610ef03f15287dc5178747d849cbe98
Joanna Broks <asia.ostr@gmail.com>
2020-05-20 15:54:59 +0000

    View for post view & delete added

--- forum/views.py
+++ forum/views.py
@@ -36,7 +36,7 @@ def add_or_edit_post(request, pk=None):
                                 post!")
             return redirect('forum')
         else:
-                messages.error(request, "Something went wrong. \
+            messages.error(request, "Something went wrong. \
                                 Please try again.")
     else:
         add_post_form = AddPostForm(instance=post)
@@ -66,3 +66,63 @@ def forum(request):
         posts = paginator.page(paginator.num_pages)
     
     return render(request, 'forum.html', {'posts': posts})
+
+def view_post(request, pk):
+    '''
+    Enables user to view page containing all details regarding a selected post
+    Function retrieves a single post based on its ID (pk) and renders it to 
+    the view_post.html template
+    If object is not found 404 error is being returned
+    '''
+    
+    # Retrive the post
+    post = get_object_or_404(Post, pk=pk)
+        
+    # Allows to retrive forms on the view post page
+    comment_post_form = AddCommentPostForm()
+    
+    # Count all commments for a post
+    comment_post_count = CommentPost.objects.filter(post=post).count()
+    
+    # Retrive all comments for a given post
+    try:
+        comments_post = CommentPost.objects.filter(post_id=post.pk)
+    except CommentPost.DoesNotExist:
+        comments_post = None
+        
+    # Paginate post comments
+    page = request.GET.get('page', 1)
+    paginator = Paginator(comments_post, 10)
+    try:
+        comments_post = paginator.page(page)
+    except PageNotAnInteger:
+        comments_post = paginator.page(1)
+    except EmptyPage:
+        comments_post = paginator.page(paginator.num_pages)
+    
+    context = {
+        'post': post,
+        'comments_post': comments_post,
+        'comment_post_form': comment_post_form,
+        'comment_post_count': comment_post_count
+    }
+    return render(request, 'view_post.html', context)
+    
+@login_required
+def delete_post(request, pk):
+    '''
+    Allows user to delete their post
+    '''
+    
+    # Retrive the post if exists
+    post = get_object_or_404(Post, pk=pk)
+    author= post.user
+    
+    if request.user.is_authenticated and request.user == author:
+            post.delete()
+            messages.success(request, "Post successfully deleted!")
+            return redirect(reverse('forum'))
+    else:
+        messages.error(request, "Error! You don't have a permission to \
+                        delete this post.")
+        return redirect('view_post', post.pk)
