a2aa49d4afe01f0cf4df09cc2899d08e03b05374
Joanna Broks <asia.ostr@gmail.com>
2020-05-20 16:30:41 +0000

    Delete and view post urls added

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from forum.views import forum, add_or_edit_post
+from forum.views import forum, add_or_edit_post, delete_post, view_post
 
 urlpatterns = [
     url(r'^$', forum, name="forum"),
     url(r'^new/post/$', add_or_edit_post, name="add_post"),
     url(r'^edit/post/(?P<pk>\d+)/$', add_or_edit_post, name="edit_post"),
+    url(r'^delete/post/(?P<pk>\d+)/$', delete_post, name="delete_post"),
+    url(r'^view/post/(?P<pk>\d+)/$', view_post, name="view_post"),
 ]
