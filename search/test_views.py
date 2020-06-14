9eba730697747174281c33fed8c10c2c885bec65
Joanna Broks <asia.ostr@gmail.com>
2020-06-14 18:30:55 +0000

    Search view tested with a query

--- search/test_views.py
+++ search/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import reverse, render, redirect
+from forum.models import Thread
 
 class TestSearchViews(TestCase):
     
@@ -9,5 +10,15 @@ class TestSearchViews(TestCase):
         URL and a correct template is used
         '''
         page = self.client.get("/search_results/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page,'search.html')
+    
+    def test_search_view_with_query_page(self):
+        '''
+        Test search view with query - if view redirects to the correct
+        URL and a correct template is used
+        '''
+        
+        page = self.client.get("/search_results/?search=Testing/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page,'search.html')
