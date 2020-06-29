3036a9145379a8999581a578f0d1b7513e44b3f9
Joanna Broks <asia.ostr@gmail.com>
2020-06-29 13:17:01 +0000

    Profile photo location path updated with user id

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+def user_directory_path(instance, filename):
+        return 'profile_image/user_{0}/{1}'.format(instance.user.id, filename)
+        
 class UserProfile(models.Model):
     '''
     Create a profile model that will enable user to upload the profile image
@@ -11,11 +14,11 @@ class UserProfile(models.Model):
     Set a placeholder image as a default
     '''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to = 'profile_image', 
+    image = models.ImageField(upload_to = user_directory_path, 
                                 default = 'profile_image/default_img.jpg',
-                                blank=True,
+                                blank = True,
                                 null = True)
-
+    
     def __str__(self):
         return f"{self.user.username}'s profile"
 
