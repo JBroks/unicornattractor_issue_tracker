9c88feb3f448e60ada2d85e072697dc1587aa869
Joanna Broks <asia.ostr@gmail.com>
2020-07-02 15:37:59 +0000

    Highcharts pie chart js added. Data to be updated

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,56 +1,102 @@
-/*
-d3.queue()
-    .defer(d3.json, "{% url 'testData' %}")
-    .await(makeGraphs);
-    
-function makeGraphs(error, data) {
-    let ticketData = crossfilter(data);
+// PIE CHART
+Highcharts.chart('container', {
+  chart: {
+    type: 'pie'
+  },
+  title: {
+    style: {
+      display: 'none'
+    }
+  },
+  subtitle: {
+    text: 'Click the slices to view ticket type breakdown by status.'
+  },
+  credits: {
+    enabled: false
+  },
+  accessibility: {
+    announceNewData: {
+      enabled: true
+    },
+    point: {
+      valueSuffix: '%'
+    }
+  },
 
-    // Call functions for each individual chart
+  plotOptions: {
+    series: {
+      dataLabels: {
+        enabled: false,
+      },
+      showInLegend: true,
+    }
+  },
 
-   showRecordsCount(ticketData);
+  tooltip: {
+    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+  },
 
-    // Render all charts
-    dc.renderAll();
-
-}
-
-
-function showRecordsCount(ticketData) {
-
-    let dim = ticketData;
-    let allRecords = ticketData.groupAll();
-
-    dc.dataCount('.dc-data-count')
-        .group(allRecords)
-        .dimension(dim);
-}
-
-
-d3.json("/testData/", function(error, data) {
-    let ticketData = crossfilter(data);
-
-    // Call functions for each individual chart
-
-    showRecordsCount(ticketData);
-
-    // Render all charts
-    dc.renderAll();
-
-});*/
-/*
-function showRecordsCount(ticketData) {
-
-    let dim = ticketData;
-    let allRecords = ticketData.groupAll();
-
-    dc.dataCount('.dc-data-count')
-        .group(allRecords)
-        .dimension(dim);
-}
-
-
-d3.json("{% url 'testData' %}", function(data) {
-    console.log(data);
+  series: [{
+    name: "Tickets",
+    colorByPoint: true,
+    data: [{
+        name: "Bugs",
+        y: 62.74,
+        drilldown: "Bugs"
+      },
+      {
+        name: "Features",
+        y: 10.57,
+        drilldown: "Features"
+      }
+    ]
+  }],
+  drilldown: {
+    series: [{
+        name: "Bugs",
+        id: "Bugs",
+        data: [
+          [
+            "Open",
+            1.02
+          ],
+          [
+            "In progress",
+            7.36
+          ],
+          [
+            "Completed",
+            0.35
+          ],
+          [
+            "Closed",
+            0.11
+          ]
+        ]
+      },
+      {
+        name: "Features",
+        id: "Features",
+        data: [
+          [
+            "Open",
+            1.02
+          ],
+          [
+            "In progress",
+            7.36
+          ],
+          [
+            "Completed",
+            0.35
+          ],
+          [
+            "Closed",
+            0.11
+          ]
+        ]
+      }
+    ]
+  }
 });
-*/
