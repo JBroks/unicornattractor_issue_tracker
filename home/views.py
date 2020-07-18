c30b909cb0ca4d2685288599ebae17070daf9ee2
Joanna Broks <asia.ostr@gmail.com>
2020-07-18 14:13:57 +0000

    testing x-frame solution

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 # Create your views here.
 
@@ -8,3 +10,7 @@ def index(request):
     A view that displays the index page
     '''
     return render(request, 'index.html')
+    
+@xframe_options_exempt
+def ok_to_load_in_a_frame(request):
+    return HttpResponse("This page is safe to load in a frame on any site.")
