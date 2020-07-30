9e5f0da69a831d7f4a04e4034c5b398e8fcbd0dc
Joanna Broks <asia.ostr@gmail.com>
2020-07-30 18:47:30 +0000

    Debug to true

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['d744010f6fbe48b2aa27987256b022c3.vfs.cloud9.us-east-1.amazonaws.com', "unicornattractor-issue-tracker.herokuapp.com"]
 
